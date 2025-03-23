64e4bb71beccae8ac8ea83e8add7df855a28c3d4
franckrm <franckrm2001@gmail.com>
2025-03-23 18:04:16 -0300

    wip: create login logic

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,12 +2,48 @@ import {Link, useNavigate} from "react-router-dom";
 import "./login.css";
 import logo from "../../assets/logo.png";
 import fundo from "../../assets/fundo.png";
+import { useState } from "react";
+import api from "../../constants/api.js"
 
 function Login(){
 
     const navigate = useNavigate();
-    function ExecuteLogin(){
-        navigate("/appointments");
+    const [email, setEmail] = useState("");
+    const [password, setPassword ] = useState(""); 
+    const [msg, setMsg ] = useState("");  
+
+    async function ExecuteLogin(){
+
+        setMsg("");
+        try{
+            const response = await api.post("/users/login", {
+                email, 
+                password
+            });
+    
+            if(response.data){
+                localStorage.setItem("sessionToken", response.data.token);
+                localStorage.setItem("sessionId", response.data.id_user);
+                localStorage.setItem("sessionEmail", response.data.email );
+                localStorage.setItem("sessionName", response.data.name);
+                navigate("/appointments");
+            }else
+                setMsg("Error ao efeturar login. Tenete novamente mais tarde");
+            
+
+        }catch (error){
+            if(error.response?.data.error)
+                setMsg(error.response?.data.error) ;
+            else
+                setMsg("Error ao efeturar login. Tenete novamente mais tarde");
+
+            console.log(error)
+        }
+
+    
+        
+        //console.log(email, password)
+        //navigate("/appointments");
     }
 
     return <div className="row">
@@ -19,15 +55,26 @@ function Login(){
 
                 <div className="mt-4">
                     <input type="email" placeholder="E-mail"
-                        className="form-control" />
+                        className="form-control" 
+                        onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="mt-2">
                     <input type="password" placeholder="Senha"
-                        className="form-control"/>
+                        className="form-control"
+                        onChange={(e)=> setPassword(e.target.value)}/>
                 </div>
                 <div className="mt-3 mb-5">
                     <button onClick={ExecuteLogin} className="btn btn-primary w-100"  type="button">Login</button>
                 </div>
+
+                {
+                    msg.length > 0 &&
+                    <div className="alert alert-danger" role="alert">
+                        {msg}
+                    </div> 
+
+                }
+
                 <div>
                     <span className="me-1">Não tenho uma conta.</span>
                     <Link to="/register">Criar agora!</Link>
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,9 +1,60 @@
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import "./register.css";
 import logo from "../../assets/logo.png";
 import fundo from "../../assets/fundo.png";
+import { useState } from "react";
+import api from "../../constants/api.js"
 
 function Register(){
+
+    const navigate = useNavigate();
+    const [name, setName ] = useState(""); 
+    const [email, setEmail] = useState("");
+    const [password, setPassword ] = useState(""); 
+    const [password2, setPassword2 ] = useState(""); 
+    const [msg, setMsg ] = useState("");  
+
+    async function ExecuteRegister(){
+    
+        setMsg("");
+
+        if(password != password2)
+            return setMsg("As senhas não conferem. Digite novamente")
+
+        try{
+            const response = await api.post("/users/register", {
+                name,
+                email, 
+                password
+            });
+    
+            if(response.data){
+                console.log(response.data)
+                localStorage.setItem("sessionToken", response.data.token);
+                localStorage.setItem("sessionId", response.data.id_user);
+                localStorage.setItem("sessionEmail", email);
+                localStorage.setItem("sessionName",name);
+                navigate("/appointments");
+            }else
+                setMsg("Error ao criar conta");
+            
+
+        }catch (error){
+            if(error.response?.data.error)
+                setMsg(error.response?.data.error) ;
+            else
+                setMsg("Error ao efeturar login. Tenete novamente mais tarde");
+
+            console.log(error)
+        }
+
+    
+        
+        //console.log(email, password)
+        //navigate("/appointments");
+    }
+    
+
     return <div className="row">
         <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
             <form className="form-signin">
@@ -13,24 +64,37 @@ function Register(){
 
                 <div className="mt-4">
                     <input type="text" placeholder="Nome"
-                        className="form-control" />
+                        className="form-control"
+                        onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div className="mt-2">
                     <input type="email" placeholder="E-mail"
-                        className="form-control" />
+                        className="form-control" 
+                        onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className="mt-2">
                     <input type="password" placeholder="Senha"
-                        className="form-control"/>
+                        className="form-control"
+                        onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className="mt-2">
                     <input type="password" placeholder="Confirme a senha"
-                        className="form-control"/>
+                        className="form-control"
+                        onChange={(e) => setPassword2(e.target.value)}/>
                 </div>
                 <div className="mt-3 mb-5">
-                    <button className="btn btn-primary w-100">Criar minha conta</button>
+                    <button onClick={ExecuteRegister}  type="button" className="btn btn-primary w-100">
+                        Criar minha conta
+                    </button>
                 </div>
+                {
+                    msg.length > 0 &&
+                    <div className="alert alert-danger" role="alert">
+                        {msg}
+                    </div> 
+
+                }
                 <div>
                     <span className="me-1">Já tenhoa uma conta.</span>
                     <Link to="/">Acessar agora!</Link>
