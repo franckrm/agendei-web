415c6321df14334d8b265403779fa9d74cd58a28
franckrm <franckrm2001@gmail.com>
2025-05-11 18:42:57 -0300

    wip: updates

--- src/pages/appointments/appointments.jsx
+++ src/pages/appointments/appointments.jsx
@@ -1,196 +1,199 @@
-
-import "./appointments.css"
+import "./appointments.css";
 import Navbar from "../../components/navbar/navbar";
 import { Link, UNSAFE_ErrorResponseImpl, useNavigate } from "react-router-dom";
 import Appointment from "../../components/navbar/appointment/appointment";
 import { useEffect, useState } from "react";
 import api from "../../constants/api";
 import { confirmAlert } from "react-confirm-alert";
-import  'react-confirm-alert/src/react-confirm-alert.css' ;  // Importar css
+import "react-confirm-alert/src/react-confirm-alert.css"; // Importar css
 
+function Appointments() {
+  const navigate = useNavigate();
+  const [appointments, setAppointments] = useState([]);
+  const [doctors, setDoctors] = useState([]);
 
-function Appointments(){
+  const [idDoctor, setIdDoctor] = useState("");
+  const [dtStart, setDtStart] = useState("");
+  const [dtEnd, setDtEnd] = useState("");
 
-    const navigate = useNavigate();
-    const [appointments, setAppointments] = useState([]);
-    const [doctors, setDoctors] = useState([]);
+  function ClickEdit(id_appointment) {
+    navigate("/appointments/edit/" + id_appointment);
+  }
 
-    const [idDoctor, setIdDoctor] = useState("");
-    const [dtStart, setDtStart] = useState("");
-    const [dtEnd, setDtEnd] = useState("");
+  function ClickDelete(id_appointment) {
+    confirmAlert({
+      title: "Exclusão",
+      message: "Confirma exclusão desse agendamento?",
+      buttons: [
+        {
+          label: "Sim",
+          onClick: () => DeleteAppointment(id_appointment),
+        },
+        {
+          label: "Não",
+          onClick: () => {},
+        },
+      ],
+    });
+  }
 
-    function ClickEdit(id_appointment){
-        navigate("/appointments/edit/"+id_appointment)
-    }
-
-    function ClickDelete(id_appointment){
-        confirmAlert({
-            title: "Exclusão",
-            message: "Confirma exclusão desse agendamento?",
-            buttons: [
-                {
-                    label: "Sim",
-                    onClick: ()=>  DeleteAppointment(id_appointment)
-                },
-                {
-                    label: "Não",
-                    onClick: ()=> {}
-                }
-            ]
-
-        });
-    }
-
-    async function DeleteAppointment(id){
-        try{
-            const response = await api.delete("/appointments/"+id);
-            if(response.data){
-               LoadAppointments();
-            }
-
-        }catch (error){
-            if(error.response?.data.error)
-                if(error.response.status == 401){
-                    if(error.response.status == 401)
-                        return navigate("/")
-                    
-                    alert(error.response?.data.error) ;
-    
-                }
-            else
-                alert("Error ao excluir dados");
-
-        }
-    } 
-
-    async function LoadDoctors(){
-        try{
-            const response = await api.get("/doctors");
-            if(response.data){
-                setDoctors(response.data);
-            }
-
-        }catch (error){
-            if(error.response?.data.error)
-                if(error.response.status == 401){
-                    if(error.response.status == 401)
-                        return navigate("/")
-                    
-                    alert(error.response?.data.error) ;
-    
-                }
-            else
-                alert("Error ao listar médicos");
-
-        }
-    } 
-
-    async function LoadAppointments(){
-        try{
-            const response = await api.get("/admin/appointments", {
-                params:{
-                    id_doctor: idDoctor,
-                    dt_start: dtStart,
-                    dt_end: dtEnd
-                }
-            });
-            if(response.data){
-                setAppointments(response.data);
-            }
-
-        }catch (error){
-            if(error.response?.data.error){
-                if(error.response.status == 401)
-                    return navigate("/")
-                
-                alert(error.response?.data.error) ;
-
-            }
-            else
-                alert("Error ao efeturar login. Tenete novamente mais tarde");
-
-        }
-    } 
-
-    function ChangeDoctor(e){
-        setIdDoctor(e.target.value);
-    }
-
-    useEffect(()=>{
-        LoadDoctors();
+  async function DeleteAppointment(id) {
+    try {
+      const response = await api.delete("/appointments/" + id);
+      if (response.data) {
         LoadAppointments();
-    },[]);
+      }
+    } catch (error) {
+      if (error.response?.data.error)
+        if (error.response.status == 401) {
+          if (error.response.status == 401) return navigate("/");
 
-    return <div className="container-fluid mt-page">
-        <Navbar/>
-        
-        <div className="d-flex justify-content-between align-items-center">
-            <div>
-                <h2 className="d-inline">Agendamentos</h2>
-                <Link to="/appointments/add" 
-                    className="btn btn-outline-primary ms-5 mb-2">
-                    Novo Agendamento
-                </Link>
-            </div>
+          alert(error.response?.data.error);
+        } else alert("Error ao excluir dados");
+    }
+  }
 
-            <div className="d-flex justify-content-end">
-                <input id="startDate" className="form-control" type="date"
-                    onChange={(e)=> setDtStart(e.target.value)} />
-                <span className="m-2">Até</span>
-                <input id="endDate" className="form-control" type="date" 
-                    onChange={(e)=> setDtEnd(e.target.value)} />
+  async function LoadDoctors() {
+    try {
+      const response = await api.get("/doctors");
+      if (response.data) {
+        setDoctors(response.data);
+      }
+    } catch (error) {
+      if (error.response?.data.error)
+        if (error.response.status == 401) {
+          if (error.response.status == 401) return navigate("/");
 
-                <div className="form-control ms-3 me-3">
-                    <select name="doctor" id="doctor" value={idDoctor} onChange={ChangeDoctor}>
-                        <option value="">Todos os médicos</option>
+          alert(error.response?.data.error);
+        } else alert("Error ao listar médicos");
+    }
+  }
 
-                        {doctors.map((doc)=>{
-                            return <option key={doc.id_doctor} 
-                                value={doc.id_doctor}>{doc.name}
-                            </option>
-                        })}
-                         
-                    </select>
-                </div>
+  async function LoadAppointments() {
+    try {
+      const response = await api.get("/admin/appointments", {
+        params: {
+          id_doctor: idDoctor,
+          dt_start: dtStart,
+          dt_end: dtEnd,
+        },
+      });
+      if (response.data) {
+        setAppointments(response.data);
+      }
+    } catch (error) {
+      if (error.response?.data.error) {
+        if (error.response.status == 401) return navigate("/");
 
-                <button onClick={(LoadAppointments)} className="btn btn-primary" type="button">Filtrar</button>
-            </div>
-        
-        </div>
+        alert(error.response?.data.error);
+      } else alert("Error ao efeturar login. Tenete novamente mais tarde");
+    }
+  }
 
+  function ChangeDoctor(e) {
+    setIdDoctor(e.target.value);
+  }
+
+  useEffect(() => {
+    LoadDoctors();
+    LoadAppointments();
+  }, []);
+
+  return (
+    <div className="container-fluid mt-page">
+      <Navbar />
+
+      <div className="d-flex justify-content-between align-items-center">
         <div>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Paciente</th>
-                        <th scope="col">Médico</th>
-                        <th scope="col">Serviço</th>
-                        <th scope="col">Data/Hora</th>
-                        <th scope="col" className="text-end">Valor</th>
-                        <th spoce="col" className="col-buttons"></th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    {
-                        appointments.map((ap)=>{
-                            return <Appointment key={ap.id_appointment} 
-                                id_appointment = {ap.id_appointment}
-                                user ={ap.user}
-                                doctor = {ap.doctor}
-                                service={ap.service}
-                                booking_date={ap.booking_date}
-                                booking_hour = {ap.booking_hour}
-                                price={ap.price}
-                                clickEdit={ClickEdit}
-                                clickDelete={ClickDelete}
-                                />
-                        })
-                    }
-                </tbody>
-            </table> 
-
+          <h2 className="d-inline">Agendamentos</h2>
+          <Link
+            to="/appointments/add"
+            className="btn btn-outline-primary ms-5 mb-2"
+          >
+            Novo Agendamento
+          </Link>
         </div>
+
+        <div className="d-flex justify-content-end">
+          <input
+            id="startDate"
+            className="form-control"
+            type="date"
+            onChange={(e) => setDtStart(e.target.value)}
+          />
+          <span className="m-2">Até</span>
+          <input
+            id="endDate"
+            className="form-control"
+            type="date"
+            onChange={(e) => setDtEnd(e.target.value)}
+          />
+
+          <div className="form-control ms-3 me-3">
+            <select
+              name="doctor"
+              id="doctor"
+              value={idDoctor}
+              onChange={ChangeDoctor}
+            >
+              <option value="">Todos os médicos</option>
+
+              {doctors.map((doc) => {
+                return (
+                  <option key={doc.id_doctor} value={doc.id_doctor}>
+                    {doc.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+
+          <button
+            onClick={LoadAppointments}
+            className="btn btn-primary"
+            type="button"
+          >
+            Filtrar
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Paciente</th>
+              <th scope="col">Médico</th>
+              <th scope="col">Serviço</th>
+              <th scope="col">Data/Hora</th>
+              <th scope="col" className="text-end">
+                Valor
+              </th>
+              <th spoce="col" className="col-buttons"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((ap) => {
+              return (
+                <Appointment
+                  key={ap.id_appointment}
+                  id_appointment={ap.id_appointment}
+                  user={ap.user}
+                  doctor={ap.doctor}
+                  service={ap.service}
+                  booking_date={ap.booking_date}
+                  booking_hour={ap.booking_hour}
+                  price={ap.price}
+                  clickEdit={ClickEdit}
+                  clickDelete={ClickDelete}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
+  );
 }
 
 export default Appointments;
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,92 +1,96 @@
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./login.css";
 import logo from "../../assets/logo.png";
 import fundo from "../../assets/fundo.png";
 import { useState } from "react";
-import api from "../../constants/api.js"
+import api from "../../constants/api.js";
 
-function Login(){
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
 
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword ] = useState(""); 
-    const [msg, setMsg ] = useState("");  
+  async function ExecuteLogin() {
+    setMsg("");
+    try {
+      const response = await api.post("/admin/login", {
+        email,
+        password,
+      });
 
-    async function ExecuteLogin(){
-
-        setMsg("");
-        try{
-            const response = await api.post("/admin/login", {
-                email, 
-                password
-            });
-    
-            if(response.data){
-                localStorage.setItem("sessionToken", response.data.token);
-                localStorage.setItem("sessionId", response.data.id_admin);
-                localStorage.setItem("sessionEmail", response.data.email );
-                localStorage.setItem("sessionName", response.data.name);
-                api.defaults.headers.common['Authorization'] = "Bearer " + response.data.token;
-                navigate("/appointments");
-            }else
-                setMsg("Error ao efeturar login. Tenete novamente mais tarde");
-            
-
-        }catch (error){
-            if(error.response?.data.error)
-                setMsg(error.response?.data.error) ;
-            else
-                setMsg("Error ao efeturar login. Tenete novamente mais tarde");
-
-            
-        }
-
-    
-        
-        //console.log(email, password)
-        //navigate("/appointments");
+      if (response.data) {
+        localStorage.setItem("sessionToken", response.data.token);
+        localStorage.setItem("sessionId", response.data.id_admin);
+        localStorage.setItem("sessionEmail", response.data.email);
+        localStorage.setItem("sessionName", response.data.name);
+        api.defaults.headers.common["Authorization"] =
+          "Bearer " + response.data.token;
+        navigate("/appointments");
+      } else setMsg("Error ao efeturar login. Tenete novamente mais tarde");
+    } catch (error) {
+      if (error.response?.data.error) setMsg(error.response?.data.error);
+      else setMsg("Error ao efeturar login. Tenete novamente mais tarde");
     }
 
-    return <div className="row">
-        <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
-            <form className="form-signin">
-                <img src={logo} className="logo mb-4" />
-                <h5 className="mb-5">Gerencie seus agendamento de forma descomplicada</h5>
-                <h5 className="mb-4 text-secondary">Acesse sua conta</h5>
+    //console.log(email, password)
+    //navigate("/appointments");
+  }
 
-                <div className="mt-4">
-                    <input type="email" placeholder="E-mail"
-                        className="form-control" 
-                        onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="mt-2">
-                    <input type="password" placeholder="Senha"
-                        className="form-control"
-                        onChange={(e)=> setPassword(e.target.value)}/>
-                </div>
-                <div className="mt-3 mb-5">
-                    <button onClick={ExecuteLogin} className="btn btn-primary w-100"  type="button">Login</button>
-                </div>
+  return (
+    <div className="row">
+      <div className="col-sm-5 d-flex justify-content-center align-items-center text-center">
+        <form className="form-signin">
+          <img src={logo} className="logo mb-4" />
+          <h5 className="mb-5">
+            Gerencie seus agendamento de forma descomplicada
+          </h5>
+          <h5 className="mb-4 text-secondary">Acesse sua conta</h5>
 
-                {
-                    msg.length > 0 &&
-                    <div className="alert alert-danger" role="alert">
-                        {msg}
-                    </div> 
+          <div className="mt-4">
+            <input
+              type="email"
+              placeholder="E-mail"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              type="password"
+              placeholder="Senha"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mt-3 mb-5">
+            <button
+              onClick={ExecuteLogin}
+              className="btn btn-primary w-100"
+              type="button"
+            >
+              Login
+            </button>
+          </div>
 
-                }
+          {msg.length > 0 && (
+            <div className="alert alert-danger" role="alert">
+              {msg}
+            </div>
+          )}
 
-                <div>
-                    <span className="me-1">Não tenho uma conta.</span>
-                    <Link to="/register">Criar agora!</Link>
-                </div>
-            </form>
-        </div>
+          <div>
+            <span className="me-1">Não tenho uma conta.</span>
+            <Link to="/register">Criar agora!</Link>
+          </div>
+        </form>
+      </div>
 
-        <div className="col-sm-7 d-flex">
-            <img src={fundo} className="background-login" />
-        </div>
+      <div className="col-sm-7 d-flex">
+        <img src={fundo} className="background-login" />
+      </div>
     </div>
+  );
 }
 
 export default Login;
